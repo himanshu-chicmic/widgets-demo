d506a9dfae1b349f92cb7601ed9f7a483db1a2a5
Himanshu Goyal <himanshu.goyal@chicmic.co.in>
2023-10-18 12:49:40 +0530

    feat(notifications, app-delegate): add app delegate to App and set delegate for local notifications.

--- WidgetsDemo/WidgetsDemoApp.swift
+++ WidgetsDemo/WidgetsDemoApp.swift
@@ -9,9 +9,51 @@ import SwiftUI
 
 @main
 struct WidgetsDemoApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
+    // app delegate:
+        
+    class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+        
+        func application(_ application: UIApplication, 
+                         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        ) -> Bool {
+            let notificationCenter = UNUserNotificationCenter.current()
+            // if You do not set delegate, does not work.
+            notificationCenter.delegate = self
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) {(accepted, _) in
+                if !accepted {
+                    print("Notification access denied")
+                }
+            }
+            
+            return true
+        }
+        
+        // called after user has tapped on notification ballon.
+        func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                    willPresent notification: UNNotification,
+                                    withCompletionHandler completionHandler:
+                                    @escaping (UNNotificationPresentationOptions) -> Void) {
+            TaskDataModel.shared.clearData { _ in
+                completionHandler([.banner, .list, .badge, .sound])
+            }
+        }
+        
+        func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                    didReceive response: UNNotificationResponse,
+                                    withCompletionHandler completionHandler: @escaping () -> Void) {
+            TaskDataModel.shared.clearData { _ in
+                completionHandler()
+            }
+        }
+    }
+    
 }
